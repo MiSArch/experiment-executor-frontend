4a7e82567807354ef8e807ed60cae2f7b3a1dfc5
eliasmueller <elias_benedikt.mueller@porsche.de>
2025-07-21 10:50:09 +0200

    fix: ensure service is available without trailing /

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/frontend/',
+  base: '/frontend',
   plugins: [vue(), tailwindcss()],
 })
